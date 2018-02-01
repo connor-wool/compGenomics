74c9e283cd231af696146497bc312439e4d2c985
connor wool <benjamin.wool@wsu.edu>
2018-02-01 00:46:41 -0800

    added simple user input processing

---
+++ alignment/alignTools.cpp
@@ -0,0 +1,13 @@
+/* -Connor Wool- -Spring 2018-
+a library file containing tools that are useful in 
+aligning two DNA sequences. Uses the Affine Align
+method.
+
+Designed to support both Global and Local alignments.
+
+*/
+
+typedef struct cell{
+    int score;
+    //other things
+}CELL;
---
+++ alignment/main.cpp
@@ -0,0 +1,46 @@
+/*
+Local and Global Affine-Allignment functions
+Written by Connor Wool
+Cpts 471, WSU, Spring 2018
+*/
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <stdlib.h>
+#include "alignTools.cpp"
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+
+    // ./a.out inputFileName <0:global 1:local> <optional:parametersFile>
+    if(argc < 3){
+        cout << "Usage: filename <FastaSequenceFile> <0:global, 1:local> <optional:parametersFile>\n";
+        cout << "Invalid Parameters entered. Exiting\n";
+        exit(1);
+    }
+    
+    string inputFileName = argv[1];
+    int userChoice;
+    stringstream(argv[2]) >> userChoice;
+    if(userChoice > 1 || userChoice < 0){
+        cout << "User option has invalid value: " << userChoice << "\n";
+        cout << "value can either be 0 (global alignment) or 1 (local alignment)\n";
+        cout << "Exiting...\n";
+        exit(1);
+    }
+
+    cout << "Sequence File: " << inputFileName << "\n";
+    cout << "global/local: " << userChoice << "\n";
+
+    string paramFileName;
+    if(argc == 4){
+        paramFileName = argv[3];
+        cout << "param file: " << paramFileName << "\n";
+    }
+
+    
+
+}
+
